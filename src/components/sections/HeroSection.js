b467d1ee0a06bc7409d9eddf5a7e97c76551e73c
CarlosDahmen <carlos.dahmen@gmail.com>
2023-03-02 14:23:23 -0500

    Add color styles, color variables and global style

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/textStyles"
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
@@ -16,16 +17,11 @@ const TextWrapper = styled.div`
   display: grid;
   gap: 30px;
 `
-const Title = styled.h1`
-  font-weight: bold;
-  font-size: 60px;
-  color: white;
+const Title = styled(H1)`
+  color: ${themes.dark.text1};
 `
 
-const Description = styled.p`
-  font-size: 17px;
-  line-height: 130%;
-`
+const Description = styled(MediumText)``
 
 const HeroSection = () => {
   return (
@@ -37,7 +33,6 @@ const HeroSection = () => {
             Design <br></br> Focused React App
           </Title>
           <Description>Welcome to your new Gatsby site.</Description>
-          {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
         </TextWrapper>
       </ContentWrapper>
     </Wrapper>

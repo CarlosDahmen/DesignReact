487fec192be6e9b40bce2a241ba507dba02d2af8
CarlosDahmen <carlos.dahmen@gmail.com>
2023-03-02 13:27:40 -0500

    Styled Components and Section Structure

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,14 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+
+const Description = styled.p`
+  font-size: 17px;
+  line-height: 130%;
+`
 
 const HeroSection = () => {
   return (
-    <div>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </div>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <img src="/images/logos/logo.svg" alt="logo" />
+          <Title>
+            Design <br></br> Focused React App
+          </Title>
+          <Description>Welcome to your new Gatsby site.</Description>
+          {/* <Link to="/page-2/">Go to page 2</Link> <br /> */}
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 

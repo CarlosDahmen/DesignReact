99cc00a26cedac5b80a8e70f3b6e359ea065c367
CarlosDahmen <carlos.dahmen@gmail.com>
2023-03-08 10:11:11 -0500

    Add CSS Selector & filter to Hover Element

--- src/components/buttons/purchaseButton.js
+++ src/components/buttons/purchaseButton.js
@@ -8,7 +8,7 @@ const PurchaseButton = ({ title, subtitle }) => {
     <Link to="page-2">
       <Wrapper>
         <IconWrapper>
-          <Icon src="/images/icons/credit.svg" />
+          <Icon src="/images/icons/credit.svg" className="icon" />
           <Ring src="/images/icons/icon-ring.svg"></Ring>
         </IconWrapper>
         <TextWrapper>
@@ -35,13 +35,21 @@ const Wrapper = styled.div`
   grid-template-columns: 53px auto;
   align-content: center;
   gap: 20px;
-  transition: 0.3s ease-in-out;
+
+  *,
+  & {
+    transition: 1s cubic-bezier(0.075, 0.82, 0.165, 1);
+  }
 
   :hover {
     box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1),
       0px 30px 60px rgba(23, 0, 102, 0.5),
       inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.5);
     transform: translateY(-3px);
+
+    .icon {
+      transform: scale(1.2);
+    }
   }
 `
 
@@ -69,6 +77,10 @@ const IconWrapper = styled.div`
   align-content: center;
   justify-self: center;
   position: relative;
+
+  ${Wrapper}:hover & {
+    filter: hue-rotate(10deg) brightness(150%) saturate(120%);
+  }
 `
 
 const TextWrapper = styled.div`
@@ -80,4 +92,8 @@ const Ring = styled.img`
   position: absolute;
   top: -15px;
   left: -16px;
+
+  ${Wrapper}:hover & {
+    transform: rotate(30deg) scale(1.2) translate(1px, 1px);
+  }
 `
